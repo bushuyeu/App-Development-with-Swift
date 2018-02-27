9c78772754729c6fe425a7be73af2c9ef26647c7
Pavel Bushuyeu <pashabushuev@gmail.com>
2018-02-27 09:25:15 +0300

    Interface Builder lab added

---
+++ Solutions/1 - Getting Started/8 - Interface Builder Basics/InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by Pavel Bushuyeu on 2/27/18.
+//  Copyright © 2018 Pavel Bushuyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
