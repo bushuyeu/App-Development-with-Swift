00db75f1e882b3629d1a82962080055e42a229ef
Pavel Bushuyeu <pashabushuev@gmail.com>
2018-03-17 12:44:47 +0300

    Calculator lab added

---
+++ Solutions/2 - Introduction to UIKit/10 - Auto Layout And Stack Views/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Pavel Bushuyeu on 3/13/18.
+//  Copyright © 2018 Pavel Bushuyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
