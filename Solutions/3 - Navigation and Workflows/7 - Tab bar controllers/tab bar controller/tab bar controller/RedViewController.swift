814fcd2e7e94d412f09de3b88e9a2a18f67e3d21
Pavel Bushuyeu <pashabushuev@gmail.com>
2018-03-26 21:30:47 +0300

    Tab bar added

---
+++ Solutions/3 - Navigation and Workflows/7 - Tab bar controllers/tab bar controller/tab bar controller/RedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tab bar controller
+//
+//  Created by Pavel Bushuyeu on 3/26/18.
+//  Copyright © 2018 Pavel Bushuyeu. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
